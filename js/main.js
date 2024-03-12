073ffdd894d74b3effb169b62a984beb30beadae
martina balanian <martinabalanian177@gmail.com>
2024-03-12 20:55:41 -0300

    agregue funciones

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-alert ("Bienvenido a Crossfit NUWA. ¿Que clase te gustaria tomar?")
-let clase = prompt("selecciona la clase que te gustaria tomar: \n1. Crossfit \n2. Funcional \n3. Gap");
+alert("Bienvenido a Crossfit NUWA");
 
-while(clase != "ESC"){
-   switch (clase) {
-      case "Crossfit":
-        alert("Felicidades, reservaste una clase de Crossfit");
-        break;
-      case "Funcional":
-        alert ("Felicidades, reservaste una clase de Funcional");
-        break;
-      case "Gap":
-        alert("Felicidades, reservaste una clase de Gap")
-        break;
-        default:
-         alert("debes ingresar una clase")   
-   }
-clase = prompt("Para continuar ingrese una clase")  
+function reservarClase() {
+  let input;
+  do {
+    input = parseInt(prompt("selecciona el numero de la clase que te gustaria tomar: \n1. Crossfit \n2. Funcional \n3. Gap"));
+    if (input > 3) {
+      alert("ingrese un numero valido")
+    }
+  } while (input > 3)
+  return input;
 }
+
+let claseUno = reservarClase();
+
+
+do {
+  if (claseUno == 1) {
+    alert ("Felicidades, reservaste una clase de Crossfit") 
+  } else if (claseUno == 2) {
+    alert ("Felicidades, reservaste una clase de Funcional")
+  } else {
+    alert ("Felicidades, reservaste una clase de Gap")
+  } 
+} while (claseUno <= 4);
+
+
